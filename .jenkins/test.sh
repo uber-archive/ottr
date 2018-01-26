2395c88eab6963d1ad9342af8ea87e411d2bc274
Keith Lea <keithl@uber.com>
2018-01-26 13:02:59 -0500

    Fix typo

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -32,12 +32,13 @@ n -q $NODE_VERSION
 # Unpm-cli will use the specified node version
 unpm install --development
 
-cp -r $(find . -maxdepth 1 -not -name .jenkins -and -not -name .git | sort) .jenkins/chrome-docker || true
+cp -r $(find . -maxdepth 1 -not -name .jenkins -and -not -name .git | sort) .jenkins/chrome-docker/ottr || true
 
 DOCKER_TAG=tag-$BUILD_TAG
 DOCKER_ID=i-$BUILD_TAG
 
-docker build -t $DOCKER_TAG .jenkins/chrome-docker/ottr
+docker build -t $DOCKER_TAG .jenkins/chrome-docker
+# /bin/bash ottr/tests-docker.sh
 docker run --name=$DOCKER_ID $DOCKER_TAG /bin/ls -lh ottr
 docker cp $DOCKER_ID:artifacts artifacts
 
