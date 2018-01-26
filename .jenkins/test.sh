40b79c9699082c562e651ae76039f3f42f635c75
Keith Lea <keithl@uber.com>
2018-01-26 10:48:43 -0500

    try running without gpu, and take screenshot to confirm it's working

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -6,7 +6,10 @@ export DEBIAN_FRONTEND=noninteractive
 
 docker build -t debian-with-chrome .jenkins/chrome-docker
 
-docker run debian-with-chrome chromium --headless https://www.chromestatus.com
+docker run debian-with-chrome chromium --headless --disable-gpu --screenshot https://time.is/
+
+mkdir -p artifacts
+mv screenshot.png artifacts
 
 # Specify Node.JS version here
 export NODE_VERSION=6.6.0
